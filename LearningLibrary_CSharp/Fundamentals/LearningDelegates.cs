2c61fa2cc946ff261280c407f27b470b73087903
Veysileth <34569591+Veysileth@users.noreply.github.com>
2021-06-24 22:54:37 +0200

    +LearningDelegates

---
+++ LearningLibrary_CSharp/Fundamentals/LearningDelegates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCsharpLibrary.Fundamentals
+{
+    public class LearningDelegates : IDemo
+    {
+        public delegate void Delegate();
+
+        private Delegate delegateFunction;
+
+        public void Demo()
+        {
+            delegateFunction += MyFirstDelegateFunction;
+            delegateFunction += MySecondDelegateFunction;
+
+            delegateFunction();
+        }
+
+        private void MyFirstDelegateFunction()
+        {
+        }
+
+        private void MySecondDelegateFunction()
+        {
+        }
+    }
+}
