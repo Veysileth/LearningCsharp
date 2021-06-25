b071796bed8c98482fccc9367495a95cf2a53b61
Veysileth <34569591+Veysileth@users.noreply.github.com>
2021-06-25 12:39:58 +0200

    LearningDelegates++

--- LearningLibrary_CSharp/Fundamentals/LearningDelegates.cs
+++ LearningLibrary_CSharp/Fundamentals/LearningDelegates.cs
@@ -18,30 +18,36 @@ namespace LearningCsharpLibrary.Fundamentals
         private BoolDelegate boolDelegateFunction;
         private BoolDelegateWithArg boolDelegateWithArgFunction;
 
+        private Action action;
+        private Action<int, float> actionIntFloat;
+
+        private Func<bool> boolFunction;
+        private Func<int, float, bool> boolFunctionIntFloat;
+
         public void Demo()
         {
             delegateFunction = MyFirstDelegateFunction;
-
             delegateFunction();
 
             delegateFunction = MySecondDelegateFunction;
+            delegateFunction();
 
+            delegateFunction = delegate () { Console.WriteLine("test1"); };
+            delegateFunction();
+
+            delegateFunction = () => { Console.WriteLine("test2"); };
             delegateFunction();
 
             boolDelegateFunction = MyFirstBoolDelegateFunction;
-
             _ = boolDelegateFunction();
 
             boolDelegateFunction = MySecondBoolDelegateFunction;
-
             _ = boolDelegateFunction();
 
             boolDelegateWithArgFunction = MyFirstBoolDelegateWithArgFunction;
-
             _ = boolDelegateWithArgFunction(1);
 
             boolDelegateWithArgFunction = MySecondBoolDelegateWithArgFunction;
-
             _ = boolDelegateWithArgFunction(2);
         }
 
