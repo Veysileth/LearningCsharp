733ac8a3eadf0bc764d8c3b6a1d4f1f24ccfc307
Veysileth <34569591+Veysileth@users.noreply.github.com>
2021-06-25 14:00:31 +0200

    LearningEvents++

--- LearningLibrary_CSharp/Fundamentals/LearningEvents.cs
+++ LearningLibrary_CSharp/Fundamentals/LearningEvents.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace LearningCsharpLibrary.Fundamentals
 {
-    class LearningEvents
+    public class LearningEvents : IDemo, INotable
     {
+        public string Note { get; } = "";
+
+        private event EventHandler OnTest;
+
+        private event EventHandler<CustomEventArgs> OnTestCustomEventArgs;
+
+        private class CustomEventArgs : EventArgs
+        {
+            public string Name { get; set; }
+        }
+
+        private void Start()
+        {
+            OnTest += LearningEvents_OnTest1;
+            OnTest += LearningEvents_OnTest2;
+            OnTestCustomEventArgs += LearningEvents_OnTestCustomEventArgs;
+        }
+
+        private void LearningEvents_OnTestCustomEventArgs(object sender, CustomEventArgs e)
+        {
+        }
+
+        private void LearningEvents_OnTest1(object sender, EventArgs e)
+        {
+        }
+
+        private void LearningEvents_OnTest2(object sender, EventArgs e)
+        {
+        }
+
+        private void Update()
+        {
+            if (true == true)
+            {
+                OnTest?.Invoke(this, EventArgs.Empty);
+                OnTestCustomEventArgs?.Invoke(this, new CustomEventArgs { Name = "test" });
+            }
+        }
+
+        public void Demo()
+        {
+            Start();
+            Update();
+        }
     }
 }
