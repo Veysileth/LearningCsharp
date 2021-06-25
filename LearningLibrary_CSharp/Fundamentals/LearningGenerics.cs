83cbea5d4149e368bc7b39c0f49377d612f70498
Veysileth <34569591+Veysileth@users.noreply.github.com>
2021-06-25 14:38:14 +0200

    +LearningGenerics++

---
+++ LearningLibrary_CSharp/Fundamentals/LearningGenerics.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCsharpLibrary.Fundamentals
+{
+    public class LearningGenerics : IDemo
+    {
+        private T[] CreateArray<T>(T first, T second)
+        {
+            return new T[] { first, second };
+        }
+
+        private void MultipleGenerics<T1, T2>(T1 first, T2 second)
+        {
+        }
+
+        public void Demo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class MyClass<T> where T : ITestableInterface
+        {
+            public T value;
+        }
+
+        private class MyClass2<T> where T : class
+        {
+            public T value;
+        }
+
+        private class MyClass3<T> where T : new()
+        {
+            public T value;
+        }
+
+        private class MyClass4<T> where T : class, ITestableInterface, new()
+        {
+            public T value;
+        }
+    }
+}
